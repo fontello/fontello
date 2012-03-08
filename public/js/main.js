3ba7e20bcb9b11474a3ee18a4a746504206f6a31
Aleksey V Zapparov <ixti@member.fsf.org>
2012-03-08 22:57:48 +0100

    Refactor main.js

--- public/js/main.js
+++ public/js/main.js
@@ -1,47 +1,32 @@
-var Fontomas = (function (Fontomas) {
+(function ($, Fontomas) {
   "use strict";
 
-  var app = Fontomas.app,
-    cfg = Fontomas.cfg,
-    env = Fontomas.env,
-    debug = Fontomas.debug,
-
-  init = function () {
-    // check browser's capabilities
-    if (!isOkBrowser()) {
-      console.log("bad browser");
-      $(cfg.id.bad_browser).modal({keyboard: false});
-      return;
-    }
-
-    // show loading tab
-    $(cfg.id.tab).tab("show");
-
-    // main model
-    app.main = new app.models.Main;
-    // main view
-    app.mainview = new app.views.Main({model: app.main});
-
-    app.mainview.render();
-  },
-
-  isOkBrowser = function () {
+  $(function () {
     // FF3.6+ Chrome6+ Opera11.1+
-    env.filereader = !!window.FileReader;
+    Fontomas.env.filereader = !!window.FileReader;
 
     // debug: simulate no filereader is available
-    if (debug.is_on && debug.nofilereader) {
-      env.filereader = false;
+    if (Fontomas.debug.is_on && Fontomas.debug.nofilereader) {
+      Fontomas.env.filereader = false;
     }
 
-    return true;
-  };
+    // check browser's capabilities
+    //if ([> check if browser is OK <]) {
+    //  console.log("bad browser");
+    //  $(Fontomas.cfg.id.bad_browser).modal({keyboard: false});
+    //  return;
+    //}
 
-  // public interface
-  return $.extend(true, Fontomas, {
-    init: init
+    // show loading tab
+    $(Fontomas.cfg.id.tab).tab("show");
+
+    // main model
+    Fontomas.app.main = new Fontomas.app.models.Main;
+    // main view
+    Fontomas.app.mainview = new Fontomas.app.views.Main({
+      model: Fontomas.app.main
+    });
+
+    Fontomas.app.mainview.render();
   });
-}(Fontomas || {}));
-
-// entry point
-$(Fontomas.init);
+}(jQuery, window.Fontomas));
