b681ff17b6658629d9bd36406b30d284bda68d7c
Roman I Shmelev <roman.i.shmelev@gmail.com>
2012-04-02 17:35:51 +0400

    eliminated initSvgFontTemplate and its dependencies

--- assets/js/fontomas/models/result_font.js
+++ assets/js/fontomas/models/result_font.js
@@ -9,55 +9,15 @@
 
   fontomas.models.result_font = Backbone.Model.extend({
     defaults: {
-      glyph_count:  0,
-      xml_template: null
+      glyph_count:  0
     },
 
 
     initialize: function () {
-      this.set("xml_template", this.initSvgFontTemplate());
       this.glyphs = new fontomas.models.glyphs_collection;
 
       this.glyphs.on("add",     this.incCounter, this);
       this.glyphs.on("remove",  this.decCounter, this);
-    },
-
-
-    initSvgFontTemplate: function () {
-      var xml_string, xml_template;
-
-      try {
-        xml_string    = $('#fm-font-output').html().trimLeft();
-        xml_template  = $.parseXML(xml_string);
-      } catch (e) {
-        fontomas.logger.error(
-          "initSvgFontTemplate: invalid xml template=",
-          $('#fm-font-output').html(),
-          "e=", e
-        );
-        fontomas.util.notify_alert("Internal error: can't parse output template.");
-        return null;
-      }
-
-      $(xml_template)
-        .find("metadata").text(config.output.metadata)
-        .end()
-        .find("font").attr({
-          "id":           config.output.font_id,
-          "horiz-adv-x":  config.output.horiz_adv_x
-        })
-        .end()
-        .find("font-face").attr({
-          "units-per-em": config.output.units_per_em,
-          "ascent":       config.output.ascent,
-          "descent":      config.output.descent
-        })
-        .end()
-        .find("missing-glyph").attr({
-          "horiz-adv-x":  config.output.missing_glyph_horiz_adv_x
-        });
-
-        return xml_template;
     },
 
 
