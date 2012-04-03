4dd21d442e6ef0538c13e4e7ef9c39c05626e775
Aleksey V Zapparov <ixti@member.fsf.org>
2012-04-03 22:32:13 +0200

    Improve glyph vlidation error

--- assets/js/fontomas/models/glyph.js
+++ assets/js/fontomas/models/glyph.js
@@ -7,14 +7,14 @@
   fontomas.models.glyph = Backbone.Model.extend({
     validate: function (attrs) {
       var c       = attrs.unicode_code,
-          ok_val  = (0      <= c && c <= 0x10ffff),
+          ok_val  = (0 <= c && c <= 0x10ffff),
           nonchar = (0xfdd0 <= c && c <= 0xfdef) ||
                     (c % 0x10000 === 0xfffe) ||
                     (c % 0x10000 === 0xffff);
 
       if (!ok_val || nonchar) {
-        fontomas.logger.debug("models.glyph.validate: bad unicode code point:", c);
-        return "Bad unicode code point " + c;
+        fontomas.logger.debug("models.glyph.validate: bad char code:", c);
+        return "Bad char code: " + c;
       }
 
       return null;
