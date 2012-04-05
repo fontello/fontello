361fc6ea320069d4fcaaf0dfed9fad4cd3f86ce9
Aleksey V Zapparov <ixti@member.fsf.org>
2012-04-05 19:00:05 +0200

    Add validator comment

--- assets/js/fontomas/models/glyph.js
+++ assets/js/fontomas/models/glyph.js
@@ -5,6 +5,16 @@
 
 
   fontomas.models.glyph = Backbone.Model.extend({
+    // SEE: http://www.unicode.org/versions/Unicode6.0.0/ch02.pdf
+    //
+    // In the Unicode Standard, the codespace consists of the integers
+    // from 0 to 10FFFF16, comprising 1,114,112 code points available for
+    // assigning the repertoire of abstract characters.
+    // ...
+    // Noncharacters. Sixty-six code points are not used to encode characters.
+    // Noncharacters consist of U+FDD0..U+FDEF and any code point ending in the
+    // value FFFE16 or FFFF16- that is, U+FFFE, U+FFFF, U+1FFFE, U+1FFFF, ...
+    // U+10FFFE, U+10FFFF.
     validate: function (attrs) {
       var c       = attrs.unicode_code,
           ok_val  = (0 <= c && c <= 0x10ffff),
