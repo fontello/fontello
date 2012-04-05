4aad248564d7bbd893f4acb49950d9357c8897f1
Aleksey V Zapparov <ixti@member.fsf.org>
2012-04-05 23:53:46 +0200

    Disable preview tab with download

--- assets/js/fontomas/ui/wizard/steps.js
+++ assets/js/fontomas/ui/wizard/steps.js
@@ -4,9 +4,16 @@
   "use strict";
 
 
+  // prevent the event from bubbling to ancestor elements
+  function stopPropagation(event) {
+    event.stopPropagation();
+  }
+
+
   fontomas.ui.wizard.steps = Backbone.View.extend({
     el: '#wizard-steps',
 
+    $preview_tab:   null,
     $result_tab:    null,
     $glyphs_count:  null,
 
@@ -14,8 +21,12 @@
       // init tabs plugin
       this.$el.tab('show');
 
+      this.$preview_tab  = this.$('a[href="#preview"]');
       this.$result_tab   = this.$('a[href="#result"]');
       this.$glyphs_count = this.$('a[href="#result"] #selected-glyphs-count');
+
+      // disable click handler of tabs plugin on preview and result tabs
+      this.$result_tab.add(this.$preview_tab).on('click', stopPropagation);
     },
 
     activate: function (id) {
@@ -23,7 +34,11 @@
     },
 
     setGlyphsCount: function (count) {
-      this.$result_tab.toggleClass('disabled', !count);
+      var $tabs = this.$result_tab.add(this.$preview_tab);
+
+      $tabs.toggleClass('disabled', !count);
+      $tabs[!count ? 'on' : 'off']('click', stopPropagation);
+
       this.$glyphs_count.text(+count);
     }
   });
