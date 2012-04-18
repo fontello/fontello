c7afe1aaaefae1509dbfb8b1b748a1b4409aa61a
Aleksey V Zapparov <ixti@member.fsf.org>
2012-04-18 15:55:13 +0200

    Add nodeca.runtime.version setter

--- fontomas.js
+++ fontomas.js
@@ -15,6 +15,13 @@ var app = NLib.Application.create({
 });
 
 
+// preset nodeca version
+nodeca.hooks.init.before('bundles', function (next) {
+  nodeca.runtime.version = require('./package.json').version;
+  next();
+});
+
+
 nodeca.hooks.init.after('bundles',        require('./lib/init/http_assets'));
 nodeca.hooks.init.after('init-complete',  require('./lib/init/http_server'));
 
