f4e10872ff90a578be53947c629eea8f950099aa
Aleksey V Zapparov <ixti@member.fsf.org>
2012-05-16 15:31:26 +0200

    Add cron for removing obsolete downloads
    Added automatic downloads cleanup (every midnight) - removing files that are
    older than 1 day since they were created.

--- fontomas.js
+++ fontomas.js
@@ -5,6 +5,11 @@
 "use strict";
 
 
+
+// 3rd-party
+var cron = require('cron');
+
+
 // nodeca
 var NLib = require('nlib');
 
@@ -47,9 +52,54 @@ nodeca.hooks.init.after('bundles', require('./lib/init/http_assets'));
 
 // Remove old downloads
 nodeca.hooks.init.before('init-complete', function (next) {
-  var downloads_path = require('path').resolve(__dirname, 'public/download');
-  nodeca.logger.warn("Cleaning obsolet downloads. Removing: " + downloads_path);
-  NLib.Vendor.FsTools.remove(downloads_path, next);
+  var downloads_path  = require('path').resolve(__dirname, 'public/download');
+
+  nodeca.logger.warn("Purging obsolet downloads. Removing: " + downloads_path);
+  NLib.Vendor.FsTools.remove(downloads_path, function (err) {
+    var job;
+
+    if (err) {
+      next(err);
+      return;
+    }
+
+    try {
+      job = new cron.CronJob({
+        // run every day at 00:00:00
+        cronTime: '0 0 0 * * *',
+        onTick: function () {
+          var now = Date.now(), // current timestamp in ms
+              max_age = 60 * 60 * 24 * 1000; // 1 day in ms
+
+          nodeca.logger.warn("Cleaning downloads...");
+          NLib.Vendor.FsTools.walk(downloads_path, function (file, stat, next) {
+            if (max_age > now - Date.parse(stat.mtime)) {
+              // file is fresh - no need to cleanup
+              nodeca.logger.debug("Skipping file: " + file);
+              next();
+              return;
+            }
+
+            nodeca.logger.warn("Removing file: " + file);
+            NLib.Vendor.FsTools.remove(file, next);
+          }, function (err) {
+            if (err) {
+              nodeca.logger.warn("Failed cleanup downloads");
+              nodeca.logger.error(err);
+            }
+          });
+        },
+        // do not start immediately
+        start: false
+      });
+    } catch (err) {
+      next(err);
+      return;
+    }
+
+    job.start();
+    next();
+  });
 });
 
 
