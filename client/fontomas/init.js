5a5217de5f962a4c79151349f53255dbc1a10543
Vitaly Puzrin <vitaly@rcdesign.ru>
2012-05-29 23:39:05 +0400

    next round of page load optimization

--- client/fontomas/init.js
+++ client/fontomas/init.js
@@ -227,4 +227,57 @@ module.exports = function () {
   nodeca.runtime.faye.subscribe('/stats/users_online', function (count) {
     $users_count.text(count);
   });
+
+
+  //
+  // Social buttons
+  //
+  $(window).load(function() {
+
+      // Google analytics
+
+      (function() {
+        var ga = document.createElement('script'); ga.type = 'text/javascript'; ga.async = true;
+        ga.src = ('https:' == document.location.protocol ? 'https://ssl' : 'http://www') + '.google-analytics.com/ga.js';
+        var s = document.getElementsByTagName('script')[0]; s.parentNode.insertBefore(ga, s);
+      })();
+
+      // Twitter buttons
+
+      !function(d,s,id) {
+        var js, fjs = d.getElementsByTagName(s)[0];
+
+        if (!d.getElementById(id)) {
+          js = d.createElement(s);
+          js.id = id;
+          js.src = "//platform.twitter.com/widgets.js";
+
+          fjs.parentNode.insertBefore(js,fjs);
+        }
+      }(document,"script","twitter-wjs");
+
+      // Google +1
+
+      (function() {
+        var po = document.createElement('script');
+
+        po.type = 'text/javascript';
+        po.async = true;
+        po.src = 'https://apis.google.com/js/plusone.js';
+
+        var s = document.getElementsByTagName('script')[0];
+        s.parentNode.insertBefore(po, s);
+      })();
+
+      // Flattr
+
+      (function() {
+        var s = document.createElement('script'), t = document.getElementsByTagName('script')[0];
+        s.type = 'text/javascript';
+        s.async = true;
+        s.src = 'http://api.flattr.com/js/0.6/load.js?mode=auto';
+        t.parentNode.insertBefore(s, t);
+      })();
+
+  });
 };
