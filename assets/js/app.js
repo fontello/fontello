e5b7554836ea73096f1dc521a93cd5907026363d
Vitaly Puzrin <vitaly@rcdesign.ru>
2012-05-30 00:50:35 +0400

    page load optimization, part 2

--- assets/js/app.js
+++ assets/js/app.js
@@ -18,3 +18,48 @@ $(function () {
   "use strict";
   nodeca.client.fontomas.init();
 });
+
+//
+// Social buttons defered load - after all
+//
+
+$(window).load(setTimeout(function() {
+
+  // Twitter buttons
+
+  !function(d,s,id) {
+    var js, fjs = d.getElementsByTagName(s)[0];
+
+    if (!d.getElementById(id)) {
+      js = d.createElement(s);
+      js.id = id;
+      js.src = "//platform.twitter.com/widgets.js";
+
+      fjs.parentNode.insertBefore(js,fjs);
+    }
+  }(document,"script","twitter-wjs");
+
+  // Google +1
+
+  (function() {
+    var po = document.createElement('script');
+
+    po.type = 'text/javascript';
+    po.async = true;
+    po.src = 'https://apis.google.com/js/plusone.js';
+
+    var s = document.getElementsByTagName('script')[0];
+    s.parentNode.insertBefore(po, s);
+  })();
+
+  // Flattr
+
+  (function() {
+    var s = document.createElement('script'), t = document.getElementsByTagName('script')[0];
+    s.type = 'text/javascript';
+    s.async = true;
+    s.src = 'http://api.flattr.com/js/0.6/load.js?mode=auto';
+    t.parentNode.insertBefore(s, t);
+  })();
+
+}, 1000));
