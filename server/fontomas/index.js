0cc437f7a9c231a767a415daba4c112fee08c442
Aleksey V Zapparov <ixti@member.fsf.org>
2012-06-05 16:43:14 +0200

    Add auto_headers middleware

--- server/fontomas/index.js
+++ server/fontomas/index.js
@@ -1,8 +1,40 @@
 /*global nodeca*/
 
+
 "use strict";
 
+
+////////////////////////////////////////////////////////////////////////////////
+
+
+function hash(str) {
+  return require('crypto').createHash('md5').update(str).digest('hex');
+}
+
+
+////////////////////////////////////////////////////////////////////////////////
+
+
+var started_at  = (new Date).toUTCString();
+var etag        = hash('fontello-layout-' + started_at);
+
+
+////////////////////////////////////////////////////////////////////////////////
+
+
 module.exports = function app(params, callback) {
+  if (!this.origin.http) {
+    callback('HTTP only');
+    return;
+  }
+
+  // set headers
+  this.response.headers['Last-Modified']  = started_at;
+  this.response.headers['ETag']           = etag;
+
+  // set view to be rendered
   this.response.view = 'layout';
+
+  // done
   callback();
 };
