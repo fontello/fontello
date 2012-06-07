5137eebe2bf3e5deba3d62d8e379385e6f634ccb
Vitaly Puzrin <vitaly@rcdesign.ru>
2012-06-08 02:54:53 +0400

    fixed flattr button protocol

--- assets/js/app.js
+++ assets/js/app.js
@@ -61,7 +61,7 @@ $(window).load(function() {
       var s = document.createElement('script'), t = document.getElementsByTagName('script')[0];
       s.type = 'text/javascript';
       s.async = true;
-      s.src = 'http://api.flattr.com/js/0.6/load.js?mode=auto';
+      s.src = '//api.flattr.com/js/0.6/load.js?mode=auto';
       t.parentNode.insertBefore(s, t);
     }());
 
