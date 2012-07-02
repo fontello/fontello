873635292e3ec241b8509742c6bfeb851615b881
Aleksey V Zapparov <ixti@member.fsf.org>
2012-07-02 22:45:17 +0200

    Set Faye timeouts

--- assets/js/nodeca-io.js
+++ assets/js/nodeca-io.js
@@ -294,7 +294,7 @@
    *  nodeca.io.init() -> Void
    **/
   io.init = function () {
-    bayeux = new Faye.Client('/faye');
+    bayeux = new Faye.Client('/faye', {timeout: 45});
 
     if ('development' === nodeca.runtime.env) {
       // export some internals for debugging
