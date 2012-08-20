37a9a13e1081c3e4c55fa7f8bc603e073c51dcc1
Aleksey V Zapparov <ixti@member.fsf.org>
2012-08-20 10:37:37 +0200

    Small improvement for unhandle excaptions handler

--- fontello.js
+++ fontello.js
@@ -27,15 +27,43 @@ nodeca.runtime.version = require('./package.json').version;
 
 
 process.on('uncaughtException', function (err) {
+  var msg, stack;
+
   try {
-    (nodeca.logger || console).warn('Uncaught exception');
-    (nodeca.logger || console).error(err.stack || err.message || err);
-  } catch (err) {
+    msg   = 'UNCAUGHT EXCEPTION !!! ' + String(err);
+    stack = err.stack || '';
+
+    if (err.original) {
+      msg += '\n' + String(err.original);
+      stack = err.original.stack || stack;
+    }
+
+    msg += '\n' + stack.split('\n').slice(1).join('\n');
+
+    nodeca.logger.fatal(msg);
+  } catch (loggerError) {
     // THIS SHOULD NEVER-EVER-EVER HAPPEN -- THIS IS A WORST CASE SCENARIO
-    // USAGE: ./fontello.js 2>/var/log/fontello-cf.log
-    process.stderr.write(err.stack || err.toString());
+    // USAGE: ./nodeca.js 2>/var/log/nodeca-cf.log
+    process.stderr.write('\nLogger failed write: ' + loggerError.stack);
+    process.stderr.write('\nOriginal error happened: ' + msg);
   }
 });
+
+
+//
+// Handle SIGnals
+//
+
+
+function shutdown_gracefully() {
+  nodeca.logger.info('Shutting down...');
+  process.exit(0);
+}
+
+
+// shutdown gracefully on SIGTERM :
+process.on('SIGTERM', shutdown_gracefully);
+process.on('SIGINT',  shutdown_gracefully);
 
 
 //
