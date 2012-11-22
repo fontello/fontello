c49aa56a91c612a2b8af42e7bba4259e7356e6bb
Aleksey V Zapparov <ixti@member.fsf.org>
2012-11-22 23:00:00 +0100

    Fix tabs

--- client/app/tabs/tabs.js
+++ client/app/tabs/tabs.js
@@ -16,7 +16,8 @@ N.once('fonts_ready', function (fontsList) {
     var
     $tabs_container = $('#tabs').tab(),
     $names_tab      = $tabs_container.find('a[href="#names-editor"]'),
-    $codes_tab      = $tabs_container.find('a[href="#codes-editor"]');
+    $codes_tab      = $tabs_container.find('a[href="#codes-editor"]'),
+    $editors        = $names_tab.add($codes_tab);
 
     // add submenu activation
     $tabs_container.find('a[data-toggle="tab"]').on('shown', function (event) {
@@ -27,14 +28,13 @@ N.once('fonts_ready', function (fontsList) {
       $($curr.data('submenu')).addClass('active');
     });
 
-    $names_tab.add($codes_tab).on('click', stopPropagation);
+    function toggleEditors(count) {
+      $editors.toggleClass('disabled', !count);
+      $editors[!count ? 'on' : 'off']('click', stopPropagation);
+    }
 
-    fontsList.selectedCount.subscribe(function (count) {
-      var $tabs = $names_tab.add($codes_tab);
-
-      $tabs.toggleClass('disabled', !count);
-      $tabs[!count ? 'on' : 'off']('click', stopPropagation);
-    });
+    toggleEditors(fontsList.selectedCount());
+    fontsList.selectedCount.subscribe(toggleEditors);
 
     // show selector tab after  load complete
     $tabs_container.find('a[href="#selector"]').tab('show');
