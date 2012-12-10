f164b23d2d81a5d86b67c2b105a93b4c31bcd66c
Aleksey V Zapparov <ixti@member.fsf.org>
2012-12-10 13:27:03 +0100

    Start work on final bundling

--- cli/server.js
+++ cli/server.js
@@ -23,11 +23,16 @@ module.exports.parserParameters = {
 
 module.exports.run = function (args, callback) {
   Async.series([
+    function (next) { N.logger.debug('Init app...'); next(); },
     require('../lib/init/app'),
 
+    function (next) { N.logger.debug('Init router...'); next(); },
     NLib.InitStages.initRouter,
 
+    function (next) { N.logger.debug('Init cronjob...'); next(); },
     require('../lib/init/cronjob'),
+
+    function (next) { N.logger.debug('Init server...'); next(); },
     require('../lib/init/server')
   ], callback);
 };
