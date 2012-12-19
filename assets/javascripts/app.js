35e7936a025e3053123fdb84819638f6fdc3b39b
Aleksey V Zapparov <ixti@member.fsf.org>
2012-12-19 18:41:59 +0100

    Replace underscore with lodash

--- assets/javascripts/app.js
+++ assets/javascripts/app.js
@@ -1,8 +1,8 @@
 //= require vendor/store
+//= require vendor/lodash
 //= require vendor/jquery/jquery
 //= require vendor/jquery-ui/jquery-ui
 //= require vendor/jquery.noty/src/js/jquery.noty
-//= require vendor/underscore
 //= require vendor/bootstrap/bootstrap
 //= require vendor/backbone
 //= require vendor/knockout
