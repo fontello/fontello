b73550cc0c80ac3637c75619bdc06ebcc7e10aa6
Aleksey V Zapparov <ixti@member.fsf.org>
2012-12-19 17:39:22 +0100

    Add router init

--- cli/server.js
+++ cli/server.js
@@ -26,9 +26,6 @@ module.exports.run = function (args, callback) {
     function (next) { N.logger.debug('Init app...'); next(); },
     require('../lib/init/app'),
 
-    function (next) { N.logger.debug('Init router...'); next(); },
-    NLib.InitStages.initRouter,
-
     function (next) { N.logger.debug('Init cronjob...'); next(); },
     require('../lib/init/cronjob'),
 
