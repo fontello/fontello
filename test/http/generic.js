8e062c7a7cfdd09f22f0b8f4dbb8f0df341cf354
Vitaly Puzrin <vitaly@rcdesign.ru>
2013-01-02 14:44:19 +0400

    added missed semicolons

--- test/http/generic.js
+++ test/http/generic.js
@@ -4,7 +4,7 @@
 'use strict';
 
 
-var request = require('supertest')('http://localhost:3000')
+var request = require('supertest')('http://localhost:3000');
 
 
 describe('Server HTTP', function () {
@@ -12,12 +12,12 @@ describe('Server HTTP', function () {
     request
       .get('/')
       .expect(/fontello/)
-      .expect(200, done)
-  })
+      .expect(200, done);
+  });
   it("/ HEAD", function (done) {
     request
       .head('/')
       .expect('')
-      .expect(200, done)
-  })
+      .expect(200, done);
+  });
 });
