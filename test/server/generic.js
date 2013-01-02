c905cf9ff51f770a15bd0aa5e5ffbc3830dffbdb
Aleksey V Zapparov <ixti@member.fsf.org>
2013-01-02 18:28:32 +0100

    fix test runner

--- test/server/generic.js
+++ test/server/generic.js
@@ -10,7 +10,7 @@ var request = require('supertest')('http://localhost:3000');
 describe('Server HTTP', function () {
   before(function (done) {
     var
-    runner      = require('../../lib.runner'),
+    runner      = require('../../lib/runner'),
     application = require('../../fontello');
 
     runner.bootstrap(application, ['server'], done);
