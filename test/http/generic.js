2401afd4e2a724d4fba25ff9e5d83bff6b65751f
Vitaly Puzrin <vitaly@rcdesign.ru>
2013-01-02 14:40:53 +0400

    reorganized tests, added http check template

---
+++ test/http/generic.js
@@ -0,0 +1,23 @@
+/*global describe, it*/
+
+
+'use strict';
+
+
+var request = require('supertest')('http://localhost:3000')
+
+
+describe('Server HTTP', function () {
+  it("/ GET", function (done) {
+    request
+      .get('/')
+      .expect(/fontello/)
+      .expect(200, done)
+  })
+  it("/ HEAD", function (done) {
+    request
+      .head('/')
+      .expect('')
+      .expect(200, done)
+  })
+});
--- test/unit/test_hooker.js
+++ test/unit/test_hooker.js
@@ -13,7 +13,7 @@ var assert = require('assert');
 
 
 // internal
-var Hooker = require('../lib/support/hooker');
+var Hooker = require('../../lib/support/hooker');
 
 
 describe('Hooker', function () {
