2a5689303ef7afc3bebc62e6e50616d72ed4c8c6
Vitaly Puzrin <vitaly@rcdesign.ru>
2013-04-11 05:59:54 +0400

    moved jquery-ui support to bower

--- assets/assets.js
+++ assets/assets.js
@@ -1,7 +1,11 @@
 //= require vendor/jquery/jquery
 //= require vendor/history.js/scripts/uncompressed/history.adapter.jquery
 //= require vendor/history.js/scripts/uncompressed/history
-//= require vendor/jquery-ui/jquery-ui
+//= require vendor/jquery-ui/ui/jquery.ui.core
+//= require vendor/jquery-ui/ui/jquery.ui.widget
+//= require vendor/jquery-ui/ui/jquery.ui.mouse
+//= require vendor/jquery-ui/ui/jquery.ui.selectable
+//= require vendor/jquery-ui/ui/jquery.ui.slider
 //= require vendor/jquery.noty/src/js/jquery.noty
 //= require vendor/bootstrap/bootstrap
 //= require vendor/store
