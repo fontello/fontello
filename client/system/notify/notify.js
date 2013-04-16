4aeb53b44b8caba3fdbd136d079228d2c6b94517
Vitaly Puzrin <vitaly@rcdesign.ru>
2013-04-16 08:23:30 +0400

    lint fixes

--- client/system/notify/notify.js
+++ client/system/notify/notify.js
@@ -8,7 +8,7 @@ N.wire.on('notify', function notify(options) {
   var msg = options;
 
   if (_.isString(options)) {
-    msg = { type: 'error', text: options }
+    msg = { type: 'error', text: options };
   }
 
   $.noty($.extend({ layout: 'topRight', theme: 'noty_theme_twitter' }, msg));
