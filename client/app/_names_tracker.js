4510e2507a38fde1f40dc0c9fc476a14efdcf65f
Dervus Grim <dervus@lavabit.com>
2013-05-15 17:43:19 +0200

    Revert last changes in the names tracker.

--- client/app/_names_tracker.js
+++ client/app/_names_tracker.js
@@ -1,49 +1,81 @@
 'use strict';
 
 
-// Hash table used to keep control on automatic name assignment when user
-// selects/deselects some glyphs. Keys are names. Values are instances of
-// GlyphModel. See app.js for details.
-var allocatedNames = {};
+////////////////////////////////////////////////////////////////////////////////
 
 
-function getFreeName(name) {
+var map       = Object(null);
+var SUFFIX_RE = /-(\d+)$/;
+
+
+////////////////////////////////////////////////////////////////////////////////
+
+
+function findFreeName(name) {
   var index = 1;
 
-  while (allocatedNames[name]) {
-    name = name.replace(/-(\d+)$/, '') + '-' + index;
-    index += 1;
+  while (map[name]) {
+    name = name.replace(SUFFIX_RE, '') + '-' + index;
+    index++;
   }
 
   return name;
 }
 
 
-function observe(glyph) {
-  // Free old name on change.
-  glyph.name.subscribe(function (name) {
-    if (allocatedNames[name] === this) {
-      allocatedNames[name] = null;
-    }
-  }, glyph, 'beforeChange');
+function allocate(model, name, prevName) {
+  var conflict = map[name];
 
-  // When user set the glyph name to a used one - rename it.
-  glyph.name.subscribe(function (name) {
-    if (this.selected()) {
-      if (allocatedNames[name]) {
-        allocatedNames[name].name(getFreeName(name));
-      }
-      allocatedNames[name] = this;
-    }
-  }, glyph);
+  if (model === map[prevName]) {
+    map[prevName] = null;
+  }
 
-  // When user deselects the glyph - free it's name.
-  glyph.selected.subscribe(function (selected) {
-    if (!selected) {
-      allocatedNames[this.name()] = null;
-    }
-  }, glyph);
+  map[name] = model;
+
+  if (conflict && conflict !== model) {
+    conflict.name(findFreeName(name));
+  }
 }
 
 
-exports.observe = observe;
+////////////////////////////////////////////////////////////////////////////////
+
+
+exports.observe = function (model) {
+  var prev = model.name();
+
+  // keep track on previous name value
+  model.name.subscribe(function (name) {
+    prev = name;
+  }, model, 'beforeChange');
+
+  // handle name change. and call allocation
+  model.name.subscribe(function (name) {
+    if (model.selected()) {
+      allocate(model, name, prev);
+    }
+  });
+
+  // keep track on selected state, before it's actual change
+  // handling beforeChange in order to be able to change name
+  // (when it's going to be selected) before allocation handler
+  model.selected.subscribe(function (value) {
+    var name = model.name();
+
+    // glyph is going to be unselected:
+    // remove from the map
+    if (value) {
+      map[name] = null;
+      return;
+    }
+
+    // glyphs is going to be selected, but it's name already "taken":
+    // find free name and update model
+    if (map[name]) {
+      name = findFreeName(name);
+      model.name(name);
+    }
+
+    map[name] = model;
+  }, model, 'beforeChange');
+};
