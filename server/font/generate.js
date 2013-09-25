1baef3ea92664370f779a201c9c056ae021d5ba7
Vitaly Puzrin <vitaly@rcdesign.ru>
2013-09-26 00:44:15 +0400

    Enchanced json schema for client config validation

--- server/font/generate.js
+++ server/font/generate.js
@@ -13,25 +13,35 @@ module.exports = function (N, apiPath) {
 
 
   N.validate(apiPath, {
-    name: {
-      type: 'string'
-    , required: false
-    }
-  , css_prefix_text: {
-      type: 'string'
-    , required: true
-    }
-  , css_use_suffix: {
-      type: 'boolean'
-    , required: true
-    }
-  , hinting: {
-      type: 'boolean'
-    , required: false
-    }
-  , glyphs: {
-      type: 'array'
-    , required: true
+    name:             { type: 'string',   required: false },
+    css_prefix_text:  { type: 'string',   required: true  },
+    css_use_suffix:   { type: 'boolean',  required: true  },
+    hinting:          { type: 'boolean',  required: false },
+    units_per_em:     { type: 'integer',  required: false, minimum: 10 },
+    ascent:           { type: 'integer',  required: false, minimum: 10 },
+    glyphs: {
+      type: 'array',
+      minItems: 1,
+      items: {
+        type: 'object',
+        additionalProperties: false,
+        properties: {
+          uid:  { type: 'string',   required: true  },
+          css:  { type: 'string',   required: true  },
+          code: { type: 'integer',  required: true  },
+          src:  { type: 'string',   required: true  },
+          svg: {
+            type: 'object',
+            required: false,
+            default: {},
+            additionalProperties: false,
+            properties: {
+              path:   { type: 'string',   required: true  },
+              width:  { type: 'string',   required: true  },
+            }
+          }
+        }
+      }
     }
   });
 
