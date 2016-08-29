d792d0a8527b84e470525c4cd2bf59a95d83e589
Kirill Efimov <kirill89@gmail.com>
2016-08-29 12:32:01 +0300

    jQuery UI Selectable -> jQuery-xselectable

--- client/fontello/blocks/selector/selector.js
+++ client/fontello/blocks/selector/selector.js
@@ -1,11 +1,11 @@
 'use strict';
 
 
-var ko = require('knockout');
+const ko = require('knockout');
 
 
 N.wire.once('navigate.done', function () {
-  var $view = $('#selector');
+  const $view = $('#selector');
 
   //
   // Bind model and view
@@ -19,17 +19,20 @@ N.wire.once('navigate.done', function () {
   //
   // Init multi-select of glyphs
   //
-  //$view.selectable({
+  //$view.xselectable({
   // We attach selectable to "body", to allow start
   // dragging from the left white space.
-  $('#selector').selectable({
+  $view.xselectable({
     filter: '.selector__glyph:visible',
-    distance: 3,
-    start() {
-      $('#selector').addClass('_multicursor');
-    },
-    stop() {
-      var $els = $view.find('.selector__glyph.ui-selected');
+    distance: 3
+  })
+    .on('xselectablestart', () => {
+      $view.addClass('_multicursor');
+    })
+    .on('xselectablestop', () => {
+      let $els = $view.find('.selector__glyph.xselectable-selected');
+
+      $els.removeClass('xselectable-selected');
 
       // prevent from double-triggering event,
       // otherwise click event will be fired as well
@@ -40,38 +43,38 @@ N.wire.once('navigate.done', function () {
       N.app.fontsList.lock();
 
       $els.each(function () {
-        var id = $(this).data('id');
-        var glyph = N.app.fontsList.getGlyph(id);
+        let id = $(this).data('id');
+        let glyph = N.app.fontsList.getGlyph(id);
 
         glyph.selected(!glyph.selected());
       });
 
       N.app.fontsList.unlock();
 
-      $('#selector').removeClass('_multicursor');
-    }
-  }).on('mousedown', () => {
-    // We should send blur manually because of issue with jQuery UI selectable,
-    // http://stackoverflow.com/questions/8869708/click-on-jquery-sortable-list-does-not-blur-input
-    $('input').blur();
-  });
+      $view.removeClass('_multicursor');
+    })
+    .on('mousedown', () => {
+      // We should send blur manually because of issue with jQuery UI selectable,
+      // http://stackoverflow.com/questions/8869708/click-on-jquery-sortable-list-does-not-blur-input
+      $('input').blur();
+    });
 
   //
   // Additionally setup class switch on mouse down/up,
   // to change cursor immediately after click on glyph
   //
-  $('#selector').on('mousedown', '.glyph', function () {
-    $('#selector').addClass('_multicursor');
+  $view.on('mousedown', '.glyph', () => {
+    $view.addClass('_multicursor');
   });
-  $('#selector').on('mouseup', function () {
-    $('#selector').removeClass('_multicursor');
+  $view.on('mouseup', () => {
+    $view.removeClass('_multicursor');
   });
 
   // Toggle glyph state on click
   //
   N.wire.on('selector:glyph_toggle', function glyph_toggle(data) {
-    var id = data.$this.data('id');
-    var glyph = N.app.fontsList.getGlyph(id);
+    let id = data.$this.data('id');
+    let glyph = N.app.fontsList.getGlyph(id);
 
     glyph.selected(!glyph.selected());
   });
@@ -79,10 +82,9 @@ N.wire.once('navigate.done', function () {
   // Toggle font collapse state on click
   //
   N.wire.on('selector:font_collapse', function font_collapse(data) {
-    var id = data.$this.data('id');
-    var font = N.app.fontsList.getFont(id);
+    let id = data.$this.data('id');
+    let font = N.app.fontsList.getFont(id);
 
     font.collapsed(!font.collapsed());
   });
-
 });
